0385b3cbfdc44163d3951a741b088e46f89a9965
jschneiderm98 <julio_schneider@hotmail.com>
2022-03-31 10:35:25 -0300

    created entities

---
+++ src/main/java/com/julio/springit/model/Comment.java
@@ -0,0 +1,20 @@
+package com.julio.springit.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Comment {
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String body;
+
+  
+}
---
+++ src/main/java/com/julio/springit/model/Vote.java
@@ -0,0 +1,21 @@
+package com.julio.springit.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Vote {
+  @Id
+  @GeneratedValue
+  private Long id;
+  
+  private int vote;
+  
+  
+}
